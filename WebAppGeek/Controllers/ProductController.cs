fe576328facae0568931db5ca4bdb9333e9d6180
ConsoleWarrior <oleg.minichev@yandex.ru>
2024-06-10 22:55:49 +0300

    Добавьте файлы проекта.

---
+++ WebAppGeek/Controllers/ProductController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebAppGeek.Data;
+using WebAppGeek.Models;
+
+namespace WebAppGeek.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ProductController : ControllerBase
+    {
+        [HttpPost]
+        public ActionResult<int> AddProduct(string name, string description, decimal price)
+        {
+            using (StorageContext storageContext = new StorageContext())
+            {
+                if (storageContext.Products.Any(p => p.Name == name))
+                    return StatusCode(409);
+
+                var product = new Product() { Name = name, Description = description, Price = price };
+                storageContext.Products.Add(product);
+                storageContext.SaveChanges();
+                return Ok(product.Id);
+            }
+        }
+        [HttpGet("get_all_products")]
+        public ActionResult<IEnumerable<Product>> GetAllProducts()
+        {
+            using(StorageContext storageContext = new StorageContext())
+            {
+                var list = storageContext.Products.Select(p => new Product{Name = p.Name, Description = p.Description, Price = p.Price}).ToList();
+                return Ok(list);
+            }
+        }
+    }
+}
